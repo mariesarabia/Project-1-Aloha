01f6ced2cdef5b2b40e4e63d19ac78e1995b39da
Marie <marie@Lawrences-Air.hitronhub.home>
2017-09-02 11:31:21 -0700

    added url source for open source code on smooth scrolling

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
-$( document ).ready(function() {
-// $(function() {
-$('.carousel').flickity({
-  // options
-  cellAlign: 'left',
- contain: true,
-  autoPlay: true,
-  prevNextButtons: false,
-});
+$(document).ready(function() {
+    // $(function() {
+    $('.carousel').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true,
+        autoPlay: true,
+        prevNextButtons: false,
+    });
 
-// Skip Link Bug Fix
-$(function() {
-   $("a[href^='#']").not("a[href='#']").click(function() {
-      $("#"+$(this).attr("href").slice(1)+"").focus();
-   });
-});
+    // Skip Link Bug Fix
+    $(function() {
+        $("a[href^='#']").not("a[href='#']").click(function() {
+            $("#" + $(this).attr("href").slice(1) + "").focus();
+        });
+    });
 
-// Add smooth scrolling to all links
-	  $("a").on('click', function(event) {
+    // Add smooth scrolling to all links (source: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll)
+    $("a").on('click', function(event) {
 
-	    // Make sure this.hash has a value before overriding default behavior
-	    if (this.hash !== "") {
-	      // Prevent default anchor click behavior
-	      event.preventDefault();
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-	      // Store hash
-	      var hash = this.hash;
+            // Store hash
+            var hash = this.hash;
 
-	      // Using jQuery's animate() method to add smooth page scroll
-	      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-	      $('html, body').animate({
-	        scrollTop: $(hash).offset().top -116
-	      }, 800, function(){
-	   
-	        // Add hash (#) to URL when done scrolling (default click behavior)
-	        window.location.hash = hash;
-	      });
-	    } // End if
-	  });
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 116
+            }, 800, function() {
 
-	$("a").on('click', function(event) {
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
 
-	});
+    $("a").on('click', function(event) {
+
+    });
 
 });
